4fceffb3d48c475ec72fa9edac265ca0223c6439
KOLPSE <117825722+KOLPSE@users.noreply.github.com>
2026-08-17 23:32:36 +0200

    0.3.7: playlists en la busqueda y filtros de preferencia

--- lib/core/app_config.dart
+++ lib/core/app_config.dart
@@ -9,7 +9,7 @@ bool get hayClientId => kDefaultClientId.isNotEmpty;
 
 const String kDiscordClientId = '1537557680024199198';
 
-const String kVersion = '0.3.6';
+const String kVersion = '0.3.7';
 
 const String kRepoGitHub = 'KOLPSE/NeoFy';
 
